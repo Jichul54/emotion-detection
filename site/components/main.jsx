f56998e8ad137ae3ea190b5b2e1579555d0506ab
Jichul54 <kjc778869@gmail.com>
2023-07-08 15:44:38 +0900

    Update Contents Using useEffect

--- site/components/main.jsx
+++ site/components/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -26,19 +26,45 @@ export default function Main() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
+  const [contents, setContents] = useState(null);
+
+  useEffect(() => {
+    // Function to set contents based on activeStep
+    const updateContents = () => {
+      if (activeStep === 0) {
+        setContents(<h3>step1</h3>);
+      } else if (activeStep === 1) {
+        setContents(<h3>step2</h3>);
+      } else if (activeStep === 2) {
+        setContents(<h3>step3</h3>);
+      } else if (activeStep === 3) {
+        setContents(<h3>step4</h3>);
+      } else if (activeStep === 4) {
+        setContents(<h3>step5</h3>);
+      } else if (activeStep === 5) {
+        setContents(<h3>step6</h3>);
+      }
+    };
+
+    updateContents(); // Call the function to set initial contents
+  }, [activeStep]);
+
   return (
     <Container maxWidth="sm">
       <Paper variant="outlined" sx={{ my: 3, p: 2 }}>
         <MyStepper steps={steps} activeStep={activeStep} />
-        <Typography variant="h5" sx={{ marginTop:2}} align="center">{steps[activeStep]}</Typography>
-        <h3 className="mt-5"> Real Time Emotion Detection</h3>
-        <img src="http://localhost:8080/video_feed" width="100%" />
+        <Typography variant="h5" sx={{ marginTop: 2 }} align="center">
+          {steps[activeStep]}
+        </Typography>
+
+        {contents}
+
         <Buttons
           steps={steps}
           activeStep={activeStep}
           handleNext={handleNext}
-                  handleBack={handleBack}
-                  style={{float: "right"}}
+          handleBack={handleBack}
+          style={{ float: "right" }}
         />
       </Paper>
     </Container>
